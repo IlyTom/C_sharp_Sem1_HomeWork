168d18cf66f17ab4b1b99e540ba961f19625acb2
IlyTom <ilytoma@mail.ru>
2023-03-08 15:52:50 +0300

    1 задача

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.Write("Введите 2 разных числа:");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+if (A > B)
+{
+    Console.WriteLine("Больше число: " + A);
+}
+else
+{
+    Console.WriteLine("Больше число: " + B);
+}
